0bbcc6f6d00c3f3c52c8938ba23653e00d5ae2f4
GertjW <58505029+GertjW@users.noreply.github.com>
2020-01-10 11:51:32 +0100

    Add files via upload
    Alle bestanden zijn toegevoegd

---
+++ Item.java
@@ -0,0 +1,19 @@
+
+/**
+ * class Objects - geef hier een beschrijving van deze class
+ *
+ * @author (jouw naam)
+ * @version (versie nummer of datum)
+ */
+public class Item
+{
+   private String itemDescription;
+   
+   public Item(String itemDescription){
+       this.itemDescription = itemDescription;
+    }
+   public String getItemDescription(){
+      return itemDescription;
+    }
+    
+}
